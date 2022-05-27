17e5c804f0b9bbd72c7dcc4fbb602871880985f7
sh4869 <sh4869@ajou.ac.kr>
2022-05-27 21:00:59 +0900

    Dynamic Link in nextjs

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 export default function Home(initialData) {
   const [formInputs, setFormInputs] = useState({})
@@ -39,6 +40,18 @@ export default function Home(initialData) {
       </form>
 
       <h1>Search results for: {searchTerm}</h1>
+
+      <p>Share this search with others:
+
+      <Link 
+        href="/search/[pid]"
+        as={`/search/${searchTerm}`}>
+        <a>
+          {`http://localhost:3000/search/${searchTerm}`}
+        </a>
+      </Link>
+
+      </p>
 
       <div className="giphy-search-results-grid">
         {searchResults.map((each, index) => {
